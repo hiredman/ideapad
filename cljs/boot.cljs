8ee93fd4ed17b91fd06973b6f37591d63433fd63
Kevin Downey <redchin@gmail.com>
2013-03-02 14:35:57 -0800

    line charts

--- cljs/boot.cljs
+++ cljs/boot.cljs
@@ -63,3 +63,101 @@
     (set! (.-onclick save-button) save))
   (when-let [f (js/eval (.-value (.getElementById js/document "javascript")))]
     (f)))
+
+(def colors
+  ["steelblue" "green" "darkorange" "PaleVioletRed" "OliveDrab" "LightSteelBlue" "Indigo"
+   "Fuchsia"])
+
+(defn ^:export color-pool []
+  (let [colors (atom (set colors))]
+    (fn [item fun]
+      (let [c (rand-nth @colors)]
+        (swap! colors disj c)
+        (fun c item)
+        item))))
+
+(defn agg-fun [data agg fun]
+  (apply agg (for [[_ vs] data
+                   v (map-indexed fun vs)]
+               v)))
+
+(defn average-dispersal [data fun]
+  (let [x (for [[_ vs] data
+                [a b] (partition 2 (map-indexed fun vs))]
+            (- a b))]
+    (/ x (count x))))
+
+(defn frame [g x y w max-y]
+  (doto g
+    (-> (.append "svg:line")
+        (.attr "x1", (x 0))
+        (.attr "y1", (* -1 (y 0)))
+        (.attr "x2", (x w))
+        (.attr "y2", (* -1 (y 0)))
+        (.style "stroke" "black"))
+    (-> (.append "svg:line")
+        (.attr "x1", (x 0))
+        (.attr "y1", (* -1 (y 0)))
+        (.attr "x2", (x 0))
+        (.attr "y2", (* -1 (y max-y)))
+        (.style "stroke" "black"))
+    (-> (.selectAll ".xLabel")
+        (.data (.ticks x 5))
+        (.enter)
+        (.append "svg:text")
+        (.attr "class" "xLabel")
+        (.text js/String)
+        (.attr "x" (fn [d] (x d)))
+        (.attr "y" 0)
+        (.attr "text-anchor" "middle"))))
+
+(defn ^:export line-chart [data {:keys [w h margin]
+                                 x-fun :x
+                                 y-fun :y}]
+  (let [max-x (agg-fun data max x-fun)
+        max-y (agg-fun data max y-fun)
+        x (-> js/d3
+              (.-scale)
+              (.linear)
+              (.domain (array 0 max-x))
+              (.range (array (+ 0 margin)
+                             (- w margin))))
+        y (-> js/d3
+              (.-scale)
+              (.linear)
+              (.domain (array 0 max-y))
+              (.range (array (+ 0 margin)
+                             (- h margin))))
+        vis (-> js/d3
+                (.select "#pad")
+                (.append "svg:svg")
+                (.attr "width" w)
+                (.attr "height" h))
+        g (-> vis
+              (.append "svg:g")
+              (.attr "transform" (str "translate(0,"h")")))
+        line (-> js/d3
+                 (.-svg)
+                 (.line)
+                 (.x (fn [d i] (x (x-fun i d))))
+                 (.y (fn [d i] (* -1 (y (y-fun i d))))))
+        line-color (color-pool)
+        color-labels (atom {})]
+    (frame g x y w max-y)
+    (doseq [[label values] data
+            :let [values (apply array values)]]
+      (-> g
+          (.append "svg:path")
+          (.attr "d" (line values))
+          (.style "fill" "none")
+          (.style "stroke-width" 2)
+          (line-color (fn [c line]
+                        (swap! color-labels assoc label c)
+                        (.style line "stroke" c)))))))
+
+;; (line-chart {"foo" [1 3 6 10 20 21 21 21 50]}
+;;             {:w 1024
+;;              :h 800
+;;              :margin 20
+;;              :x (fn [i d] i)
+;;              :y (fn [i d] d)})
